ad60863b17558160a127b7ccb3f52621af6dfbb4
student <michal21399@gmail.com>
2024-10-31 12:31:41 +0200

    my commit message

---
+++ Entities/AddFlight.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1.Entities
+{
+    public class AddFlight
+    {
+        private  DateOnly date{ get; set; }
+        private string destination {  get; set; }
+        private int department{ get; set; }
+        private int ChairPosition { get; set; }
+        private bool fly {  get; set; }
+        private bool hotel {  get; set; }
+        private bool trip { get; set; }
+
+
+    }
+
+}
---
+++ Entities/CreditCard.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Entities
+{
+    public class CreditCard
+    {
+        private long creditNumber;//מספר אשראי
+        private DateOnly validity;//תוקף
+        private int cvv;//3 ספרות בגב הכרטיסcvv
+        private long tz;//תעודת זהות
+
+    }
+}
